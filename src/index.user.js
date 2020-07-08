ab3a5c75102c5091ad4dafdd71b6ed6c88cf3ba2
humberd <humberd2@gmail.com>
2020-07-08 23:08:23 +0200

    Added better description

--- src/index.user.js
+++ src/index.user.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name		  eRepublik Stuff++ Unlocked
+// @description An unlocked version of stuff++ (https://docs.google.com/spreadsheets/d/1nal62cgC7lUmrur6NRzlPVU3uxtE59WGV9-bZcPoIw8/edit#gid=0), that for some reason didn't want to run after Zordacz ban.
 // @author		Zordacz, Humberd
 // @version		5.39
 // @match		  https://www.erepublik.com/*
