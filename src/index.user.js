30e8e4f23311517121267f4b04e13c71e895a61b
Humberd <humberd.dev@gmail.com>
2023-06-29 21:10:10 +0200

    Redo name change

--- src/index.user.js
+++ src/index.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name		  eRepublik Stuff++ Unlocked2
+// @name		  eRepublik Stuff++ Unlocked
 // @description An unlocked version of stuff++ (https://docs.google.com/spreadsheets/d/1nal62cgC7lUmrur6NRzlPVU3uxtE59WGV9-bZcPoIw8/edit#gid=0), that for some reason didn't want to run after Zordacz ban.
 // @author		Zordacz, Humberd
 // @version		5.55
