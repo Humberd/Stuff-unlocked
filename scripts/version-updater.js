1c5f98365be87257d45bac9918fcaae4a902bbde
Humberd <humberd.dev@gmail.com>
2023-11-15 22:13:46 +0100

    Misc: Automatic version updater script with GH Actions

--- scripts/version-updater.js
+++ scripts/version-updater.js
@@ -78,8 +78,34 @@ function prependChangesToChangelog(version, changes, date) {
   fs.writeFileSync(path, updatedChangelog);
 }
 
-function doAll() {
+async function outputVersionToGithubAction(newVersion, changes) {
+  if (process.env.GITHUB_ACTIONS !== "true") {
+    console.log("Not running outside GitHub Actions. Skipping setting output.");
+    return;
+  }
+  await installDependencies();
+
+  const core = require("@actions/core");
+  core.setOutput("newVersion", newVersion);
+  core.setOutput("changes", "* " + changes.join("\n* "));
+}
+
+async function installDependencies() {
+  const util = require("util");
+  const jsExec = util.promisify(require("child_process").exec);
+
+  console.log("Installing npm dependencies");
+  const { stdout, stderr } = await jsExec(
+    "npm install @actions/core --no-save"
+  );
+  console.log("npm-install stderr:\n\n" + stderr);
+  console.log("npm-install stdout:\n\n" + stdout);
+  console.log("Finished installing npm dependencies");
+}
+
+async function doAll() {
   const newVersion = bumpCurrentVersion(Type.minor);
+  console.log(`Bumping version to ${newVersion}`);
   updateAllVersions(newVersion);
 
   const currentDate = new Date()
@@ -89,11 +115,13 @@ function doAll() {
     .reverse()
     .join(".");
 
-  prependChangesToChangelog(
-    newVersion,
-    getAllCommitsSinceLastBump(),
-    currentDate
-  );
+  const allCommitsSinceLastBump = getAllCommitsSinceLastBump();
+  console.log("All commits since last bump:");
+  console.log(allCommitsSinceLastBump);
+
+  prependChangesToChangelog(newVersion, allCommitsSinceLastBump, currentDate);
+
+  await outputVersionToGithubAction(newVersion, allCommitsSinceLastBump);
 }
 
 doAll();
