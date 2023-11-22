7a8b33740b6b5718e364b7f0b45c6d886c5aebcf
Humberd <humberd.dev@gmail.com>
2023-11-22 23:17:38 +0100

    Revert "Autoformat"
    This reverts commit 260a5789c86d7accc86edd159a9b9b81c48570a1.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
-import { log, addLocationChangeCallback } from "./utils/utils";
+import App from "./App";
+import { awaitElement, log, addLocationChangeCallback } from "./utils/utils";
 import "./old-index.user.js";
 
 log("React script has successfully started");
@@ -8,24 +11,28 @@ log("React script has successfully started");
 // so that elements can be re-inserted as a user navigates a page with
 // different routes.
 async function main() {
-  // Find <body/>. This can be any element. We wait until
-  // the page has loaded enough for that element to exist.
-  // const body = await awaitElement("body");
+    // Find <body/>. This can be any element. We wait until
+    // the page has loaded enough for that element to exist.
+    const body = await awaitElement("body");
+    const container = document.createElement("div");
+    body.appendChild(container);
+    ReactDOM.render(<App />, container);
 }
 
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", watchForUrlChange);
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', watchForUrlChange);
 } else {
-  watchForUrlChange();
+    watchForUrlChange();
 }
 
 function watchForUrlChange() {
-  // Call `main()` every time the page URL changes, including on first load.
-  addLocationChangeCallback(() => {
-    // Greasemonkey doesn't bubble errors up to the main console,
-    // so we have to catch them manually and log them
-    main().catch((e) => {
-      log(e);
+    // Call `main()` every time the page URL changes, including on first load.
+    addLocationChangeCallback(() => {
+        // Greasemonkey doesn't bubble errors up to the main console,
+        // so we have to catch them manually and log them
+        main().catch((e) => {
+            log(e);
+        });
     });
-  });
+
 }
