2fadf3dd77e1566442e9d799697bee91407b6c21
Humberd <humberd.dev@gmail.com>
2025-01-28 00:02:55 +0100

    Improve comment

--- src/features/improved-storage/index.tsx
+++ src/features/improved-storage/index.tsx
@@ -5,8 +5,10 @@ export const ImprovedStorage = createFeature({
   name: "Improved Storage",
   canExecute: (url) => url.includes("/main/inventory"),
   execute: async () => {
-    // adding a class only for this view, so that styles won't override anything else on otherviews,
-    // because the styles are loaded for all the views
+    /**
+     * All the styles are loaded and applied immediately for all the views.
+     * Adding this class make these static styles scoped, so that they only apply to this view.
+     */
     document.body.classList.add("su-improved-storage")
   },
 });
