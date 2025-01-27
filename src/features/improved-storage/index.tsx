b9ce9e8bd8c3377107efe4ffe42b8e42d51ba881
Humberd <humberd.dev@gmail.com>
2025-01-27 23:58:26 +0100

    Made inventory more compact

--- src/features/improved-storage/index.tsx
+++ src/features/improved-storage/index.tsx
@@ -5,6 +5,8 @@ export const ImprovedStorage = createFeature({
   name: "Improved Storage",
   canExecute: (url) => url.includes("/main/inventory"),
   execute: async () => {
+    // adding a class only for this view, so that styles won't override anything else on otherviews,
+    // because the styles are loaded for all the views
     document.body.classList.add("su-improved-storage")
   },
 });
