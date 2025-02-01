a131236367b686e8f0ac08c559268e42f8dd9cab
Humberd <humberd.dev@gmail.com>
2025-02-01 13:35:15 +0100

    Fix: disable autocapture in analytics

--- src/analytics/posthog.ts
+++ src/analytics/posthog.ts
@@ -20,6 +20,7 @@ export const Analytics = {
       person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
       capture_pageview: false,
       capture_pageleave: "if_capture_pageview",
+      autocapture: false,
     });
     isInitialized = true;
   },
