c822e7c029918ac2c1da4b92c186252aeaa1236b
Humberd <humberd.dev@gmail.com>
2025-02-01 21:35:38 +0100

    Feat: Add blinking color for items expiring in 1 day in side inventory

--- src/features/side-inventory/components/SideInventory.tsx
+++ src/features/side-inventory/components/SideInventory.tsx
@@ -19,7 +19,11 @@ export const SideInventory: React.FC<SideInventoryProps> = (props) => {
   return (
     <div className={styles.SideInventory}>
       {props.items.map((item) => {
-        const tooltip = `${item.attributes?.duration ? `[${item.attributes.duration}] `: ""}${item.name}`
+        const isExpiringSoon = (item.attributes?.temporary?.expiresIn ?? Infinity) < 60 * 60 * 24; // 1 day
+        let tooltip = `${item.attributes?.duration ? `[${item.attributes.duration}] `: ""}${item.name}`
+        if (isExpiringSoon) {
+          tooltip += `\nExpires in less then 1 day!!!`;
+        }
         return (
           <div key={item.id} className={styles.Item} title={tooltip}>
             <img className={styles.Img} src={item.icon} alt={item.name} />
@@ -27,6 +31,7 @@ export const SideInventory: React.FC<SideInventoryProps> = (props) => {
               className={classNames(styles.Text, {
                 [styles.IsActive]: item.attributes?.active,
                 [styles.IsTemporary]: item.attributes?.temporary,
+                [styles.ExpiresSoon]: isExpiringSoon,
               })}
             >
               {item.amountDisplay}
