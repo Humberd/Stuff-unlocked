fb46e3a05b450582a9f4c8d2158c69239e62a0d5
Humberd <humberd.dev@gmail.com>
2026-04-14 10:33:51 +0200

    Misc: Add type definitions for SCSS and CSS modules

--- styles.d.ts
+++ styles.d.ts
@@ -2,3 +2,13 @@ declare module '*.module.scss' {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module '*.scss' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
